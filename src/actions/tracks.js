1740ccfcda72f8386e467140d2d41db0c03bfc07
igor <ig1985@ukr.net>
2018-10-20 15:40:29 +0300

    last commit from tutorial

--- src/actions/tracks.js
+++ src/actions/tracks.js
@@ -1,8 +1,29 @@
-export const getTracks = () => {
-    return dispatch => {
-        setTimeout(() => {
-            console.log('I got tracks');
-            dispatch({ type: 'FETCH_TRACKS_SUCCESS', payload: [] });
-        }, 2000)
-    }
+var mockApiData = [
+  {
+    id: 1,
+    name: 'Enter Sandman'
+  },
+  {
+    id: 2,
+    name: 'Welcome Home'
+  },
+  {
+    id: 3,
+    name: 'Master of Puppets'
+  },
+  {
+    id: 4,
+    name: 'Fade to Black'
+  },
+  {
+    id: 5,
+    name: 'Nothing Else Matters'
+  }
+];
+
+export const getTracks = () => dispatch => {
+  setTimeout(() => {
+    console.log('I got tracks');
+    dispatch({type: 'FETCH_TRACKS_SUCCESS', payload: mockApiData});
+  }, 2000)
 };
